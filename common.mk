4c016c4755d1831ae9349f3f7ab269c552d19f7b
Maitani-Sakura <xjichuan479@gmail.com>
2024-06-11 20:29:21 +0900

    Add "android.hardware.nfc@1.2-service"

--- common.mk
+++ common.mk
@@ -367,6 +367,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw \
     vendor.nxp.hardware.nfc@2.0-service \
+    android.hardware.nfc@1.2-service \
     android.hardware.secure_element@1.0.vendor \
     android.hardware.secure_element@1.1.vendor \
     android.hardware.secure_element@1.2.vendor
